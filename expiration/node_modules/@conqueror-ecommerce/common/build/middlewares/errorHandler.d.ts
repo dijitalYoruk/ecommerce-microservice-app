444b5b7d1ae4f20cf86afee25154c7a6d78d0971
Fatih Sevban Uyanik <fatihsevban15@gmail.com>
2021-05-10 01:20:18 +0300

    node modules deleted

--- expiration/node_modules/@conqueror-ecommerce/common/build/middlewares/errorHandler.d.ts
+++
@@ -1,2 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const errorHandler: (err: any, req: Request, res: Response, next: NextFunction) => Response<any, Record<string, any>>;
